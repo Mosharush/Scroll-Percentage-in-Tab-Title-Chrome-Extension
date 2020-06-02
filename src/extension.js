0601ad249f3a66d7337fb15e94684abbdd345e33
Moshe Harush <mosheh@yad2.co.il>
2020-06-02 14:51:21 +0300

    Scroll event as intersection observer

--- src/extension.js
+++ src/extension.js
@@ -4,35 +4,44 @@ console.log("Scroll Percentage in Tab Title Extension loaded 👍");
 
 const originalTitle = document.title;
 
-window.addEventListener(
-  "scroll",
-  () => {
-    const windowHeight = document.body.offsetHeight;
-    const scrollTop = window.scrollY;
-    const containers = document.querySelectorAll(
-      "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
-    );
+function findOutMainContentElement() {
+  const scrollTop = window.scrollY;
+  const documentHeight = document.body.offsetHeight;
+  const containers = document.querySelectorAll(
+    "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
+  );
 
-    let mainContainer = null,
-      mainContainerSizes = null;
+  let mainContainer = null,
+    mainContainerSizes = null;
 
-    for (let x = containers.length - 1; x >= 0; x--) {
-      mainContainer = containers[x];
-      mainContainerSizes = mainContainer.getBoundingClientRect();
-      if (windowHeight / mainContainerSizes.height < 3) {
-        break;
-      }
+  for (let x = containers.length - 1; x >= 0; x--) {
+    mainContainer = containers[x];
+    mainContainerSizes = mainContainer.getBoundingClientRect();
+    if (documentHeight / mainContainerSizes.height < 3) {
+      break;
     }
+  }
+  return {
+    scrollTop,
+    mainContainer,
+    mainContainerSizes,
+  };
+}
 
-    let docHeight =
-      scrollTop + mainContainerSizes.top + mainContainerSizes.height;
-    let winHeight = window.innerHeight;
-    let scrollPercent = scrollTop / (docHeight - winHeight);
-    let scrollPercentRounded = Math.min(
-      Math.max(Math.round(scrollPercent * 100), 0),
-      100
-    );
-    document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
-  },
-  { passive: true }
-);
+let { mainContainer } = findOutMainContentElement();
+let observer = new IntersectionObserver((entries, observer) => {
+  const { scrollTop, mainContainerSizes } = findOutMainContentElement();
+
+  let docHeight = mainContainerSizes.top + mainContainerSizes.height;
+  let winHeight = window.innerHeight;
+  let scrollPercent =
+    scrollTop > docHeight ? 100 : scrollTop / (docHeight - winHeight);
+  let scrollPercentRounded = Math.min(
+    Math.max(Math.round(scrollPercent * 100), 0),
+    100
+  );
+  document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
+});
+mainContainer.querySelectorAll("p").forEach((elem) => {
+  observer.observe(elem);
+});
