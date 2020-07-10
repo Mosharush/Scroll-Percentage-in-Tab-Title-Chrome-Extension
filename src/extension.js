e5c81cc40b72e39dc329e00eec3d5b09a95af577
Moshe Harush <upload770@gmail.com>
2020-07-10 18:47:40 +0300

    Rebase merge feature branch fix

--- src/extension.js
+++ src/extension.js
@@ -5,33 +5,39 @@ console.log("Scroll Percentage in Tab Title Extension loaded 👍");
 const originalTitle = document.title;
 
 function findOutMainContentElement() {
-  const scrollTop = window.scrollY;
-  const documentHeight = document.body.offsetHeight;
+  const windowHeight = window.innerHeight;
   const containers = document.querySelectorAll(
     "body,main,*[class~='content'],*[class*='Content'],*[class~='article']:first-child,article:first-child"
   );
 
-  let mainContainer = null,
-    mainContainerSizes = null;
+  let bestMatchedContainer = {
+    ratio: 0,
+    elm: null,
+  };
 
   for (let x = containers.length - 1; x >= 0; x--) {
-    mainContainer = containers[x];
-    mainContainerSizes = mainContainer.getBoundingClientRect();
-    if (documentHeight / mainContainerSizes.height < 3) {
-      break;
-    }
+    const optionalMainContainer = containers[x];
+    const optionalMainContainerSizes = optionalMainContainer.getBoundingClientRect();
+    const containerRatio = windowHeight / optionalMainContainerSizes.height;
+
+    bestMatchedContainer.ratio <= containerRatio &&
+      (bestMatchedContainer = {
+        ratio: containerRatio,
+        elm: optionalMainContainer,
+        sizes: optionalMainContainerSizes,
+      });
   }
-  return {
-    scrollTop,
-    mainContainer,
-    mainContainerSizes,
-  };
+
+  return bestMatchedContainer;
 }
 
-let { mainContainer } = findOutMainContentElement();
-let observer = new IntersectionObserver((entries, observer) => {
-  const { scrollTop, mainContainerSizes } = findOutMainContentElement();
+let {
+  elm: mainContainer,
+  sizes: mainContainerSizes,
+} = findOutMainContentElement();
 
+let observer = new IntersectionObserver((entries, observer) => {
+  const scrollTop = window.scrollY;
   let docHeight = mainContainerSizes.top + mainContainerSizes.height;
   let winHeight = window.innerHeight;
   let scrollPercent =
